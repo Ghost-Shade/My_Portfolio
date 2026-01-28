6a90db7883162b12ca7df9e8e0f0a25a82448983
Jamson Anjera <jamsonanjera@gmail.com>
2026-01-28 11:39:17 +0300

    Build for Github pages

---
+++ docs/assets/useScrollToSection-B1rC0EvZ.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-BAqNtJwl.js";const l=()=>({scrollToSection:t.useCallback(e=>{const o=document.getElementById(e);o&&o.scrollIntoView({behavior:"smooth"})},[])});export{l as u};
